29d23baa6b900e14b1e27e04bcf7c2f9893513d6
George Wortmann <gf.wortmann@gmail.com>
2022-11-02 00:23:28 +0300

    task #10 committed

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//
+//
+//entering block/////////////а
+int intN1, intResult; 
+//
+Console.Clear();
+Console.Write("Введите 3-значное число ");
+intN1 = int.Parse(Console.ReadLine()!);
+//
+// end of entering block/////////////
+//
+//
+//exec block
+//
+intResult = intN1 % 100 / 10;
+Console.WriteLine($"{intResult}");
+//
+// end of exec block
